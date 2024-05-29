b8f0140dc41c9a43b70671d4bf722cfe821a8e49
Dawid Kogut <kogutdawid@gmail.com>
2024-05-29 18:55:33 +0200

    Add guaranted chars to password

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,33 @@ function App() {
     const passLength = passParams.passLength;
     let characters = lowerCaseLetters;
     let newPassword = "";
-    passParams.isSpecialChars ? (characters += specialChars) : "";
-    passParams.isNumbers ? (characters += numbers) : "";
-    passParams.isBigLetters ? (characters += upperCaseLetters) : "";
+    let guaranteedCharacters = ""; // gwarantowane znaki, które wystąpią w haśle
 
-    for (let i = 0; i < passLength; i++) {
+    if (passParams.isSpecialChars) {
+      characters += specialChars;
+      guaranteedCharacters +=
+        specialChars[Math.floor(Math.random() * specialChars.length)];
+    }
+
+    if (passParams.isNumbers) {
+      characters += numbers;
+      guaranteedCharacters +=
+        numbers[Math.floor(Math.random() * numbers.length)];
+    }
+
+    if (passParams.isBigLetters) {
+      characters += upperCaseLetters;
+      guaranteedCharacters +=
+        upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
+    }
+
+    newPassword += guaranteedCharacters;
+
+    for (let i = guaranteedCharacters.length; i < passLength; i++) {
       newPassword += characters[Math.floor(Math.random() * characters.length)];
     }
     setPassword(newPassword);
   }
-  console.log(`password: ${password}`);
   return (
     <>
       <div className={styles.container}>
